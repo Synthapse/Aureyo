9abb405ce31615ce504a8e5d5f113c0b046e3896
PiotrZak <37672365+PiotrZak@users.noreply.github.com>
2025-04-18 15:50:45 +0200

    fix

--- Aureyo.Frontend/src/index.tsx
+++ Aureyo.Frontend/src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import MainLayout from 'layouts/MainLayout';
+import Home from 'pages/Home';
+import HowItWorks from 'pages/HowItWorks';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import theme from 'styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +16,16 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-      <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <MainLayout>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+          </Routes>
+        </HashRouter>
+      </MainLayout>
+    </ThemeProvider>
   </React.StrictMode>
 ); 
